cab31deabc88bf26fa37a2393a8f9f01104e2087
Shane Peelar <lookatyouhacker@gmail.com>
2017-09-27 12:21:00 -0400

    Update Travis CI test for "mv" overlay dependency
    Will need work.  But should get things working at least.

--- tests/repoman.sh
+++ tests/repoman.sh
@@ -5,6 +5,17 @@ set -ex
 # Disable news messages from portage and disable rsync's output
 export FEATURES="-news" PORTAGE_RSYNC_EXTRA_OPTS="-q"
 
+mkdir -p /etc/portage/repos.conf
+
+cat << EOF >> /etc/portage/repos.conf/mv.conf
+[mv]
+location = /usr/local/mv
+sync-type = git
+sync-uri = https://anongit.gentoo.org/git/user/mv.git
+auto-sync = Yes
+sync-depth = 1
+EOF
+
 # Update the portage tree and install dependencies
 emerge --sync
 emerge -1 portage

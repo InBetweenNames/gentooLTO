643779b1b972c565f465ea97742a50505ad951d2
Shane Peelar <lookatyouhacker@gmail.com>
2018-10-27 19:09:57 -0400

    bashrc.d/42-lto-flag-o-matic.sh: Override more
    Also override filter-flags and append-flags.
    Add an override for glibc to enable it to build with
    full optimizations, minus LTO.  LTO does not appear
    to be supported in glibc yet.
    
    Signed-off-by: Shane Peelar <lookatyouhacker@gmail.com>

--- sys-config/ltoize/files/bashrc.d/42-lto-flag-o-matic.sh
+++ sys-config/ltoize/files/bashrc.d/42-lto-flag-o-matic.sh
@@ -9,8 +9,21 @@ LTOOverrideFlagOMatic()
 	{
 		ewarn "lto-overlay: replace-flags OVERRIDDEN"
 	}
+
+	append-flags()
+	{
+		ewarn "lto-overlay: append-flags OVERRIDDEN"
+	}
+
+	filter-flags()
+	{
+		ewarn "lto-overlay: filter-flags OVERRIDDEN"
+	}
 }
 
+
+BashrcdPhase setup LTOOverrideFlagOMatic
+BashrcdPhase unpack LTOOverrideFlagOMatic
 BashrcdPhase prepare LTOOverrideFlagOMatic
 BashrcdPhase configure LTOOverrideFlagOMatic
 BashrcdPhase compile LTOOverrideFlagOMatic
